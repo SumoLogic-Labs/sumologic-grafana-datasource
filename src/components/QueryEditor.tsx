4a2651ba59280f182928b356f2a015776346be22
Anton Kononenko <akononenko@sumologic.com>
2023-04-04 11:15:50 +0200

    Fix README file

--- src/components/QueryEditor.tsx
+++ src/components/QueryEditor.tsx
@@ -7,19 +7,21 @@ import { SumoQuery } from '../types/metricsApi.types';
 type Props = QueryEditorProps<DataSource, SumoQuery>;
 
 export function QueryEditor({ query, onChange, onRunQuery }: Props) {
-  const onQueryTextChange = (changedQueryText: string) => {
+  const onQueryTextChange = React.useCallback((changedQueryText: string) => {
     onChange({ ...query, queryText: changedQueryText });
-  };
+  }, [query]);
 
   const { queryText } = query;
 
   return (
     <div style={{ display: 'flex', gap: '1rem' }}>
       <QueryField
+        // if onRunQuery is not passed then you user would not be able to focus out from the QueryField
+        onRunQuery={onRunQuery}
         onChange={onQueryTextChange}
         placeholder="Metric Query"
         query={queryText || ''}
-        portalOrigin="plugin"
+        portalOrigin="sumo-metrics"
       />
       <Button onClick={onRunQuery}>Run</Button>
     </div>
